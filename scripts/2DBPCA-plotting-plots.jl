355b2cb36607132e742c98b45fd73b466e38d665
Clarence Ioakim Sy <syioakim@gmail.com>
2023-11-19 12:08:02 +0800

    Re-coded plotting and animation script

--- scripts/2DBPCA-plotting-plots.jl
+++ scripts/2DBPCA-plotting-plots.jl
@@ -12,18 +12,7 @@ begin
     using Profile
 end
 
-begin
-    # List of parameters
-    sizes = [128]
-    seat_configs = ["random"]
-    Λs = [0.25,0.5,0.75]
-    steady_state_tolerance = 10
-    n_trials = 3
-    n_learned = 4
-end
-
-
-function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Vector{Float64}, steady_state_tolerance::Int, n_trials::Int; n_learned::Int=4)
+function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Vector{Float64}, steady_state_tolerance::Int, n_trials::Int; n_learned::Int=4, animation::Bool=false)
     default()
     l = @layout [
                 a{0.75h}
@@ -34,6 +23,8 @@ function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Ve
     for trial in 1:n_trials
         for seat_config in seat_configs, λ₀ in Λs, class_size in sizes#, trial in 1:n_trials
             
+            frames = []
+
             println("$seat_config 	$λ₀ 	$class_size 	$trial")
 
             if seat_config == "random"
@@ -53,7 +44,7 @@ function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Ve
             generations = [reshape(generations[i], (class_size,class_size)) for i in 1:num_generations]
 
             learned = params_df[!,"learned_per_gen"]
-            learned_dom = 1:length(learned)
+            #! Check if deletable: learned_dom = 1:length(learned)
             power_coeff, power_exp = params_df[!,"power_fit"][1:2]
             fit_dom = 1:0.1:length(learned)
             fit_vals = power_coeff .* fit_dom .^ power_exp
@@ -101,7 +92,7 @@ function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Ve
 
                 learned_plot = vline!([length(generations) - Int64(floor(0.25*length(learned)))], ls = :dash, label = "End of fit data")
 
-                _plot_title = seat_config == "random" ? "Classroom over time\n$(seat_config)-$(size)-$(n_learned)-$(λ₀)" : "Classroom over time\n$(seat_config)-$(size)-$(λ₀)"
+                _plot_title = seat_config == "random" ? "Classroom over time\n$(seat_config)-$(class_size)-$(n_learned)-$(λ₀)" : "Classroom over time\n$(seat_config)-$(class_size)-$(λ₀)"
 
                 _plot = plot(class_plot,learned_plot, 
                     layout = l,
@@ -110,14 +101,42 @@ function generate_plots(sizes::Vector{Int}, seat_configs::Vector{String},Λs::Ve
                     plot_title = _plot_title
                 );
 
+                if animation == true
+                    push!(frames,_plot)
+                end
+
                 if seat_config == "random"
                     savefig("./output/2D-Binary-PCA/random-$(class_size)-$(n_learned)/$(λ₀)/trial_$(trial)/images/2DBPCA-random-$(class_size)-$(n_learned)-$(λ₀)-$(i).png")
                 else
-                    savefig("./output/2D-Binary-PCA/random-$(class_size)/$(λ₀)/trial_$(trial)/images/2DBPCA-random-$(class_size)-$(λ₀)-$(i).png")
+                    savefig("./output/2D-Binary-PCA/$(seat_config)-$(class_size)/$(λ₀)/trial_$(trial)/images/2DBPCA-random-$(class_size)-$(λ₀)-$(i).png")
+                end # frame save end
+
+            end # per generation plot end
+
+            if animation == true
+                anim = @animate for i in 1:length(frames)
+                    plot(frames[i]);
                 end
+
+                if seat_config == "random"
+                    mp4(anim, "./output/2D-Binary-PCA/random-$(class_size)-$(n_learned)/$(λ₀)/trial_$(trial)/2DBPCA-random-$(class_size)-$(n_learned)-$(λ₀)-animation.mp4", fps = 16);
+                else
+                    mp4(anim, "./output/2D-Binary-PCA/$(seat_config)-$(class_size)/$(λ₀)/trial_$(trial)/2DBPCA-random-$(class_size)-$(λ₀)-animation.mp4", fps = 16);
+                end # animation save end
             end
-        end
-    end
+
+        end # params end
+    end # trial iteration end
+end # function end
+
+begin
+    # List of parameters
+    sizes = [32,64,128]
+	seat_configs = ["outer_corner","inner_corner","center"]
+	Λs = collect(0.2:0.2:1)
+	steady_state_tolerance = 10
+	n_trials = 3
+    n_learned = 4
 end
 
-@time generate_plots(sizes,seat_configs, Λs, steady_state_tolerance, n_trials; n_learned = n_learned)
+generate_plots(sizes,seat_configs, Λs, steady_state_tolerance, n_trials; n_learned = n_learned, animation = true)
